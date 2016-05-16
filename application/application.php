2fb32ac1bdacce702c689b3ffe70c4d04b875ed1
pianorockcover <pianorockcover@gmail.com>
2016-05-16 23:13:12 +0300

    Написал руты

--- application/application.php
+++ application/application.php
@@ -8,21 +8,27 @@ class Application
 	function __construct($config)
 	{
 		# Проверить запрос на ниличие инъекций
+		# Проверить запрос на соответствие шаблону
 
 		ConfigRegistry::init($config);
 		QueryRegistry::init($_SERVER['REQUEST_URI']);
 
-		exit();
-		
 		return;
 	}
 
 	public function run()
 	{
-		$this->router = new Router();
-		$controller = $router->getController();
-		$action = $action->getAction();
-		$controller->$action();
+		$this->router = new Router(QueryRegistry::getInstance()->getQuery());
+		
+		$controller = $this->router->getController();
+		$action = $this->router->getAction();
+		$params = $this->router->getParams();
+
+		echo '<br>'.$controller.'<br>';
+		echo $action.'<br>';
+		var_dump($params);
+
+		// $controller->$action($params);
 
 		return;
 	}
--- application/router.php
+++ application/router.php
@@ -5,15 +5,53 @@ class Router
 {
 	private $controller;
 	private $action;
+	private $actionParams;
 
 	function __construct($query)
 	{
 		$this->controller = 'site';
 		$this->action = 'index';
-		echo $query;
+		$this->actionParams = NULL;
+
+		# Парсим запросы типа: ?r=controler/action&param1=value&param2=value...
+		$query = parse_url($query);
+
+		if (isset($query['query'])) 
+		{
+			$params = [];
+			foreach (explode('&', $query['query']) as $paramName => $paramValue) {
+				$params[explode('=', $paramValue)[0]] = explode('=', $paramValue)[1];
+			}
+
+			$actionAndController = $params['r'];
+
+			$this->controller = ucfirst(substr($actionAndController, 0, strpos($actionAndController, '/'))).'Controller';
+			
+			$this->action = 'action'.ucfirst(substr($actionAndController, strpos($actionAndController, '/') + 1, strlen($actionAndController)));	
+
+			if(count($params) > 1)
+			{
+				unset($params['r']);
+				$this->actionParams = $params; 
+			}
+
+		}
 
 		return;
-		#Проверка url
-		#Parse Query;
+	}
+
+	public function getController()
+	{
+		return $this->controller;
+	}
+
+	public function getAction()
+	{
+		return $this->action;
+	}
+
+	public function getParams()
+	{
+		return $this->actionParams;
 	}
 }
