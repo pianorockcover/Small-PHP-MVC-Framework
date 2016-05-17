88d0ce8a5d8ad70bab5d30178093be985b422207
pianorockcover <pianorockcover@gmail.com>
2016-05-17 11:07:26 +0300

    Добавлены виджеты (в частности пагинация)

---
+++ application/widget.php
@@ -0,0 +1,7 @@
+<?php
+namespace application;
+
+abstract class Widget
+{
+	abstract public function widget(...$params);
+}
---
+++ widgets/pagination.php
@@ -0,0 +1,12 @@
+<?php
+namespace widgets;
+
+use \application\Widget;
+
+class Pagination extends Widget
+{
+	function widget(...$params)
+	{
+		# code...
+	}
+}
