deb57f4831d639bd85b68cfc2a0db543ec461385
pianorockcover <pianorockcover@gmail.com>
2016-05-18 21:08:55 +0300

    Пофиксил баг с удалением новостей

--- controllers/newsController.php
+++ controllers/newsController.php
@@ -109,6 +109,18 @@ class NewsController extends Controller
 		$news->query("DELETE FROM {$news->table()} 
 								  WHERE news.news_id = {$params['news_id']}");
 		$news->execute();
+
+		$news->query("SELECT * FROM news; ");
+		$amountOfRecords = $news->amountOfRecords();
+
+		for ($i = $params['news_id']; $i <= $amountOfRecords; $i++) { 
+			$next = $i + 1;
+			$news->query("UPDATE news
+								SET news.news_id = {$i}
+									WHERE news.news_id = {$next}; ");
+			$news->execute();
+		}
+
 		if(file_exists("assets/images/{$params['news_id']}.jpg"))
 		{
 			unlink("assets/images/{$params['news_id']}.jpg");
