0c64a6e6f63cc2c568fa3b413781e4da701eb804
pianorockcover <pianorockcover@gmail.com>
2016-05-18 14:56:48 +0300

    Пофиксил парсинг

--- controllers/parseController.php
+++ controllers/parseController.php
@@ -12,15 +12,34 @@ class ParseController extends Controller {
 
 		$site = iconv(mb_detect_encoding($site, mb_detect_order(), true), "UTF-8", $site);
 
-		preg_match_all('/<div class="news-record record_feed_list">(.*)<\/div>/U', $site, $allNews);
+		preg_match_all('/<div class="news-record record_feed_list">(.*)<\/noindex>/U', $site, $allNews);
 
 		$allNews = $allNews[1];
+		var_dump($allNews);
 		$upToDateNews = [];
 		foreach ($allNews as $news) {
 			$date = NULL;
 			preg_match_all('/<span class="title">(.*)<\/span>/U', $news, $date);
 			$date = $date[1][0];
-			array_push($upToDateNews, ['date' => $date]);
+
+			# Меняем формат даты и проверяем ее
+
+			$title = NULl;
+			preg_match_all('/<span class="title2" >	(.*)<\/span>/U', $news, $title);
+			$title = $title[1][0];
+
+			$summary = NULl;
+			$news = substr($news, strpos($news, '<noindex>'));
+			preg_match_all('/<noindex>(.*)$/U', $news, $summary);
+
+			$summary = strip_tags($summary[1][0]);
+
+			array_push($upToDateNews, ['date' => $date,
+										'title' => $title,
+										'summary' => $summary,
+										]);
+
+
 		}
 
 		var_dump($upToDateNews);
--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 ini_set('xdebug.var_display_max_depth', 9);
 ini_set('xdebug.var_display_max_children', 1256);
 ini_set('xdebug.var_display_max_data', 2024);
-
+ini_set('pcre.backtrack_limit', '5000000');
 mb_internal_encoding("UTF-8");
 use application\Application;
 
